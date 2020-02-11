10f230d093e518f8fc3230926144f99269afcf5b
Aluno <rafael.zottesso@ifpr.edu.br>
2020-02-10 22:45:29 -0300

    Criação e configuração do projeto Django.

---
+++ Progressao/paginas/views.py
@@ -0,0 +1,12 @@
+
+# Importar o TemplateView para criar páginas simples
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+
+# A classe PaginaInicial "extends" TemplateView
+class PaginaInicial(TemplateView):
+    # Toda classe filha do TemplateView precisa do
+    # atributo abaixo para definir um template a ser renderizado
+    template_name = 'paginas/index.html'
