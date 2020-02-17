24338787ff02a3afe14afd53e59f6fc5d4b89aee
Aluno <rafael.zottesso@ifpr.edu.br>
2020-02-17 20:40:54 -0300

    Adição do jQuery, Bootstrap e Font Awesome 4.7.

--- Progressao/paginas/views.py
+++ Progressao/paginas/views.py
@@ -10,3 +10,6 @@ class PaginaInicial(TemplateView):
     # Toda classe filha do TemplateView precisa do
     # atributo abaixo para definir um template a ser renderizado
     template_name = 'paginas/index.html'
+ 
+class SobreView(TemplateView):
+    template_name = 'paginas/sobre.html'
