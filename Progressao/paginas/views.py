6525273de142c87163d1b0fd4fb555a14755a487
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2020-03-26 12:13:02 -0300

    Formatação do template modelo.

--- Progressao/paginas/views.py
+++ Progressao/paginas/views.py
@@ -9,7 +9,7 @@ from django.views.generic import TemplateView
 class PaginaInicial(TemplateView):
     # Toda classe filha do TemplateView precisa do
     # atributo abaixo para definir um template a ser renderizado
-    template_name = 'paginas/index2.html'
+    template_name = 'paginas/index.html'
  
 class SobreView(TemplateView):
     template_name = 'paginas/sobre.html'
